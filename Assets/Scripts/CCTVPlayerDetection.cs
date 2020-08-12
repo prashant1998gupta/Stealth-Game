b1badfe8f248259345f8e66747dbe5a0775b7469
Prashant <prashant1998gupta@gamil.com>
2020-08-12 19:29:28 +0530

    cctv half setting donw WIP

---
+++ Assets/Scripts/CCTVPlayerDetection.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CCTVPlayerDetection : MonoBehaviour
+{
+    public GameObject player;
+    public LastPlayerSighting lastPlayerSighting;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag(Tags.player);
+        lastPlayerSighting = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<LastPlayerSighting>();
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        if(collider.gameObject == player)
+        {
+            Vector3 relPosPlayer = player.transform.position - transform.position;
+            RaycastHit hit;
+
+            if(Physics.Raycast(transform.position , relPosPlayer , out hit))
+            {
+                if (hit.collider.gameObject == player)
+                {
+                    lastPlayerSighting.position = player.transform.position;
+                }
+            }
+        }
+    }
+}
