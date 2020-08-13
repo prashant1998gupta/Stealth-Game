d409000618b63256614834e7d2f3101348ef93a6
Prashant <prashant1998gupta@gamil.com>
2020-08-14 00:00:41 +0530

    laser grid completed and bake lightmap

---
+++ Assets/Scripts/LaserBlink.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBlink : MonoBehaviour
+{
+    public float onTime;
+    public float offTime;
+
+    private float timer;
+    bool rendere;
+    bool lighting;
+
+    void Awake()
+    {
+        bool renderer = GetComponent<MeshRenderer>().enabled;
+        bool light = GetComponent<Light>().enabled;
+    }
+
+    
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(rendere && timer >= onTime)
+        {
+            SwitchBeam();
+        }
+
+        if(!rendere && timer >= offTime)
+        {
+            SwitchBeam();
+        }
+
+
+    }
+
+    private void SwitchBeam()
+    {
+        rendere = !rendere;
+        lighting = !lighting;
+    }
+}
